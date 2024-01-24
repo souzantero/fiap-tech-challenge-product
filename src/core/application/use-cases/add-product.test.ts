fc2386628a7fc7b50e72d647fa1b763ed853acd3
Felipe Antero <souzantero@gmail.com>
2024-01-23 22:53:04 -0300

    setup jest

---
+++ src/core/application/use-cases/add-product.test.ts
@@ -0,0 +1,27 @@
+import { ProductInMemoryDatabase } from "../../../main/databases/in-memory/product-in-memory-database";
+import { ProductType } from "../../domain/entities/product";
+import { AddProduct } from "./add-product";
+
+const randomString = (length = 10) => Math.random().toString(36).substring(length);
+const randomInt = (min = 0, max = 100) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+
+describe('AddProduct', () => {
+  it('should add a product', async () => {
+    // Arrange
+    const productRepository = new ProductInMemoryDatabase()
+    const addProduct = new AddProduct(productRepository);
+    const data = {
+      type: ProductType.Drink,
+      name: randomString(),
+      description: randomString(),
+      price: randomInt(),
+    };
+
+    // Act
+    const product = await addProduct.addOne(data);
+
+    // Assert
+    expect(product).toHaveProperty('id');
+  });
+});
